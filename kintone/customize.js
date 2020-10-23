347412e831ce366cb7c53e660e8a1d78bd8122a8
uchan-nos <uchan0@gmail.com>
2020-10-23 22:44:02 +0900

    add kintone customize script

---
+++ kintone/customize.js
@@ -0,0 +1,17 @@
+(function() {
+  const wasmPath = 'https://uchan-nos.github.io/rust-wasm-kintone/pkg/rust-wasm-kintone_bg.wasm';
+
+  kintone.events.on('app.record.detail.show', (event) => {
+    (async () => {
+      console.log('initializing wasm');
+      await wasm_bindgen.wasm_bindgen(wasmPath);
+
+      const greetMsg = wasm_bindgen.greet(event.record['name']['value']);
+      console.log(greetMsg);
+
+      const greetEl = kintone.app.record.getSpaceElement('greet');
+      const textEl = document.createElement('P');
+      text.innerText = greetMsg;
+      greetEl.appendChild(textEl);
+    })();
+}());
