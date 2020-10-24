5fa1dfd182181919290c1c40a4b89497fa3599e9
uchan-nos <uchan0@gmail.com>
2020-10-24 10:51:55 +0900

    simplify customize javascript

--- kintone/customize.js
+++ kintone/customize.js
@@ -1,18 +1,16 @@
 (function() {
   const wasmPath = 'https://uchan-nos.github.io/rust-wasm-kintone/pkg/rust-wasm-kintone_bg.wasm';
 
-  kintone.events.on('app.record.detail.show', (event) => {
-    (async () => {
-      console.log('initializing wasm');
-      await wasm_bindgen(wasmPath);
+  kintone.events.on('app.record.detail.show', async (event) => {
+    console.log('initializing wasm');
+    await wasm_bindgen(wasmPath);
 
-      const greetMsg = wasm_bindgen.greet(event.record['name']['value']);
-      console.log(greetMsg);
+    const greetMsg = wasm_bindgen.greet(event.record['name']['value']);
+    console.log(greetMsg);
 
-      const greetEl = kintone.app.record.getSpaceElement('greet');
-      const textEl = document.createElement('P');
-      textEl.textContent = greetMsg;
-      greetEl.appendChild(textEl);
-    })();
+    const greetEl = kintone.app.record.getSpaceElement('greet');
+    const textEl = document.createElement('P');
+    textEl.textContent = greetMsg;
+    greetEl.appendChild(textEl);
   });
 }());
